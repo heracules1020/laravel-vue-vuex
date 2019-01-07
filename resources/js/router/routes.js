aa680d70efbfb5e01eb793407f56d8a1514aa537
heracules1020 <heracules1020@outlook.com>
2019-01-07 05:19:02 -0800

    first commit

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -3,12 +3,14 @@ function page (path) {
 }
 
 export default [
-  { path: '/', name: 'welcome', component: page('welcome.vue') },
+  
 
   { path: '/login', name: 'login', component: page('auth/login.vue') },
-  { path: '/register', name: 'register', component: page('auth/register.vue') },
+  { path: '/', name: 'welcome', component: page('welcome.vue') },
   { path: '/password/reset', name: 'password.request', component: page('auth/password/email.vue') },
   { path: '/password/reset/:token', name: 'password.reset', component: page('auth/password/reset.vue') },
+  { path: '/register', name: 'register', component: page('auth/register.vue') },
+
   { path: '/email/verify/:id', name: 'verification.verify', component: page('auth/verification/verify.vue') },
   { path: '/email/resend', name: 'verification.resend', component: page('auth/verification/resend.vue') },
 
