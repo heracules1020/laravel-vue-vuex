e37f17845fd469c2b48b18c4bf6efa2112e57378
heracules1020 <heracules1020@outlook.com>
2019-02-11 06:57:04 -0700

    register verifcation

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -8,39 +8,39 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    use ResetsPasswords;
+	use ResetsPasswords;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
 
-    /**
-     * Get the response for a successful password reset.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  string  $response
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function sendResetResponse(Request $request, $response)
-    {
-        return ['status' => trans($response)];
-    }
+	/**
+	 * Get the response for a successful password reset.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  string  $response
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	protected function sendResetResponse(Request $request, $response)
+	{
+		return ['status' => trans($response)];
+	}
 
-    /**
-     * Get the response for a failed password reset.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  string  $response
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function sendResetFailedResponse(Request $request, $response)
-    {
-        return response()->json(['email' => trans($response)], 400);
-    }
+	/**
+	 * Get the response for a failed password reset.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  string  $response
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	protected function sendResetFailedResponse(Request $request, $response)
+	{
+		return response()->json(['email' => trans($response)], 400);
+	}
 }
